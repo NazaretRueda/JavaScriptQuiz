95d58459ffdfd5e4de0afa14a7b499fe8f12ebcb
Naz <150092323+Naz19914@users.noreply.github.com>
2024-01-02 19:02:44 +0000

    An array of questions

---
+++ assets/js/questions.js
@@ -0,0 +1,62 @@
+let quizQuestions = [
+    {
+        question: "Question 1: What does JavaScript primarily add to a web page?",
+        answer: {
+            a: "Style",
+            b: "Interactivity",
+            c: "Structure",
+            d: "Images",
+        },
+        correctAnswer: "b: Interactivity"
+    },
+    {
+        question: "Question 2: Which operator is used for equality without type coercion in JavaScript?",
+        answer: {
+            a: "===",
+            b: "==",
+            c: "=",
+            d: "!==",
+        },
+        correctAnswer: "a: ==="
+    },
+    {
+        question: "Question 3: Which function is used to add a new element at the end of an array in JavaScript?",
+        answer: {
+            a: "push()",
+            b: "pop()",
+            c: "shift()",
+            d: "unshift()",
+        },
+        correctAnswer: " a: push()"
+    },
+    {
+        question: "Question 4: What does the addEventListener() method do in JavaScript?",
+        answer: {
+            a: "Adds a new element to the page",
+            b: "Adds a click event to an element",
+            c: "Changes the page's background color",
+            d: "Removes an element from the page",
+        },
+        correctAnswer: "b: Adds a click event to an element"
+    },
+    {
+        question: "Question 5: How do you declare a function in JavaScript?",
+        answer: {
+            a: "function myFunction() {}",
+            b: "var myFunction = function() {}",
+            c: "let myFunction = () => {}",
+            d: "All of the above",
+        },
+        correctAnswer: "d: All of the above"
+    },
+    {
+        question: "Question 6: What is the purpose of the Array.isArray() method in JavaScript?",
+        answer: {
+            a: "To check if a variable is an array",
+            b: "To add an element to an array",
+            c: "To remove the first element from an array",
+            d: "To convert an object to an array",
+        },
+        correctAnswer: "a: To check if a variable is an array"
+    },
+];
